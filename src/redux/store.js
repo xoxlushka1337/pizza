228dc2e83522b02e59e9c4c34e311bada479744d
xoxlushka1337 <xoxlushka@gmail.com>
2023-10-24 23:54:14 +0300

    feat: add Redux

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import filterSlice from "./slices/filterSlice";
+
+export const store = configureStore({
+	reducer: {
+		filterSlice,
+	},
+});
+
+console.log(configureStore);
