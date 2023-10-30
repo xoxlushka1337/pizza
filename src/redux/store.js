45aeb3cc86aabea63beade913cc89569f280657b
xoxlushka1337 <ulana.xoxlushka@gmail.com>
2023-10-30 19:20:23 +0300

    fix: will take out Redux logic for the basket

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import filterSlice from "./slices/filterSlice";
+import cartSlice from "./slices/cartSlice";
 
 export const store = configureStore({
 	reducer: {
 		filterSlice,
+		cartSlice,
 	},
 });
-
-console.log(configureStore);
